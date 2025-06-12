fd021ba263b2f7f4a9cf3b3b02693da0ee65e141
nicollylosnaque <nicolly.l.nunes@aluno.senai.br>
2025-06-12 09:11:58 -0300

    focus e envio com enter

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ const taskList = document.getElementById('taskList');
 const titulo = document.getElementById('titulo');
 
 let nome = prompt("Qual o seu nome?")
+tarefa.focus();
 titulo.innerHTML = `Lista de tarefa: ${nome}`;
 
 btnAdd.addEventListener("click",criaTarefa);
+
+//Envio com o Enter
+tarefa.addEventListener('keypress', function(e){
+    if (e.key === 'Enter') criaTarefa();
+});
+//
+
 function criaTarefa(){
+    if(tarefa.value==""){
+        alert("digite o nome da sua tarefa")
+    } else{
+
+    
     const listItem = document.createElement('li')
     listItem.textContent = tarefa.value;
     taskList.appendChild(listItem);
@@ -38,5 +51,10 @@ function criaTarefa(){
         listItem.classList.toggle('completed');
     })
 
+    //Apaga input apos envio
+    tarefa.value = '';
+    tarefa.focus(); //retorma o foco para o campo de entrada
+
+        }
 
 }
